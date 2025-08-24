e88a86551543971edae69edd82c23b0f6ed606e4
arshan404 <fcshayan20@gmail.com>
2025-08-24 12:03:10 +0330

    OOP PART 6

--- Chapter 2 An Overview of Python/oop overview.py
+++ Chapter 2 An Overview of Python/oop overview.py
@@ -1,15 +1,15 @@
 class Human:
-    pass
+    def __init__(self , name):
+        self.name = name
 
-p1 = Human()
-p2 = Human()
+p1 = Human('ali')
+#p2 = Human()
 
-p1.name = 'ali'
-p1.lname  = 'bigdeli'
+#p1.name = 'ali'
+#p1.lname  = 'bigdeli'
 
-p2.name = 'hassan'
-p2.lname = 'jabari'
-p2.email = 'asfaf@gmail.com'
+#p2.name = 'hassan'
+#p2.lname = 'jabari'
 
-#print(p1.email)
-print(p2.email)
+print(p1.name)        #چون داخل کلاس تعریف شده پس برای هردو داره ایجاد میشه
+##print(p2.manager)
