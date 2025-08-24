c25246cb7b3cf3d535bc6223e178773feae68ec5
arshan404 <fcshayan20@gmail.com>
2025-08-24 11:55:33 +0330

    oop part 4 atribute

--- Chapter 2 An Overview of Python/oop overview.py
+++ Chapter 2 An Overview of Python/oop overview.py
@@ -4,11 +4,14 @@ class Human:
 p1 = Human()
 p2 = Human()
 
+p1.name = 'ali'
+p1.lname  = 'bigdeli'
+
 p1_name = 'ali'
 p1_lname = 'bigdeli'
 
 p2_name = 'hassan'
 p2_lname = 'jabari'
 
-print(p1)
+print(p1.name)
 print(p2)
