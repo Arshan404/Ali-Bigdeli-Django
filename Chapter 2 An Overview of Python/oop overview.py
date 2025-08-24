01a6d06c938893bdfcf4042592b4287eaef55d69
arshan404 <fcshayan20@gmail.com>
2025-08-24 11:54:24 +0330

    oop part three object adress created

--- Chapter 2 An Overview of Python/oop overview.py
+++ Chapter 2 An Overview of Python/oop overview.py
@@ -2,6 +2,7 @@ class Human:
     pass
 
 p1 = Human()
+p2 = Human()
 
 p1_name = 'ali'
 p1_lname = 'bigdeli'
@@ -10,5 +11,4 @@ p2_name = 'hassan'
 p2_lname = 'jabari'
 
 print(p1)
-print(p1_name)
-print(p1_lname)
+print(p2)
